bf40b0a1571fd94b50f524c3ecbd50efd85d12bc
axross <kou@axross.org>
2015-11-14 21:46:01 +0900

    Add codes

---
+++ distributions/index.js
@@ -0,0 +1,76 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _nodeNotifier = require('node-notifier');
+
+var _nodeNotifier2 = _interopRequireDefault(_nodeNotifier);
+
+var _through2 = require('through2');
+
+var _through22 = _interopRequireDefault(_through2);
+
+var _tapParser = require('tap-parser');
+
+var _tapParser2 = _interopRequireDefault(_tapParser);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var DEFAULT_PASSED_OPTIONS = {
+  title: 'Test passed.',
+  icon: _path2['default'].resolve(__dirname, '../passed.png'),
+  sound: false
+};
+
+var DEFAULT_FAILED_OPTIONS = {
+  title: 'Test failed!',
+  icon: _path2['default'].resolve(__dirname, '../failed.png'),
+  sound: 'Basso'
+};
+
+var createReporter = function createReporter() {
+  var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+  var passed = _ref.passed;
+  var failed = _ref.failed;
+
+  var passedOptions = Object.assign({}, DEFAULT_PASSED_OPTIONS, passed);
+  var failedOptions = Object.assign({}, DEFAULT_FAILED_OPTIONS, failed);
+  var p = (0, _tapParser2['default'])();
+  var stream = (0, _through22['default'])(function (chunk, enc, next) {
+    this.push(chunk);
+    next();
+  });
+  var errorOccuredAt = null;
+
+  stream.pipe(p);
+
+  p.on('assert', function (assert) {
+    if (assert.ok) return;
+
+    errorOccuredAt = assert.diag.at;
+  });
+
+  p.on('complete', function (result) {
+    if (result.ok) {
+      _nodeNotifier2['default'].notify(Object.assign({
+        message: result.pass + ' of ' + result.count + ' tests passed.'
+      }, passedOptions));
+    } else {
+      _nodeNotifier2['default'].notify(Object.assign({
+        message: result.fail + ' of ' + result.count + ' tests failed at ' + errorOccuredAt
+      }, failedOptions));
+    }
+  });
+
+  return stream;
+};
+
+exports['default'] = createReporter;
+module.exports = exports['default'];
---
+++ sources/index.js
@@ -0,0 +1,51 @@
+import notifier from 'node-notifier';
+import through2 from 'through2';
+import parser from 'tap-parser';
+import path from 'path';
+
+const DEFAULT_PASSED_OPTIONS = {
+  title: 'Test passed.',
+  icon: path.resolve(__dirname, '../passed.png'),
+  sound: false,
+};
+
+const DEFAULT_FAILED_OPTIONS = {
+  title: 'Test failed!',
+  icon: path.resolve(__dirname, '../failed.png'),
+  sound: 'Basso',
+};
+
+const createReporter = ({ passed, failed } = {}) => {
+  const passedOptions = Object.assign({}, DEFAULT_PASSED_OPTIONS, passed);
+  const failedOptions = Object.assign({}, DEFAULT_FAILED_OPTIONS, failed);
+  const p = parser();
+  const stream = through2(function(chunk, enc, next) {
+    this.push(chunk);
+    next();
+  });
+  let errorOccuredAt = null;
+
+  stream.pipe(p);
+
+  p.on('assert', assert => {
+    if (assert.ok) return;
+
+    errorOccuredAt = assert.diag.at
+  });
+
+  p.on('complete', result => {
+    if (result.ok) {
+      notifier.notify(Object.assign({
+        message: `${result.pass} of ${result.count} tests passed.`,
+      }, passedOptions));
+    } else {
+      notifier.notify(Object.assign({
+        message: `${result.fail} of ${result.count} tests failed at ${errorOccuredAt}`,
+      }, failedOptions));
+    }
+  });
+
+  return stream;
+};
+
+export default createReporter;
